3189e635816eb38eac5c111f640df1e1c728c562
Zander Hill <zhill@rentpath.com>
2014-05-07 15:26:44 -0400

    Worser code

--- pr_test.rb
+++ pr_test.rb
@@ -1,7 +1,9 @@
 module Pr
+
   class Test
-    def method_to_overwrite(args)
-      do_something_else_with(args)
+
+    def method_to_overwrite(  args )
+      do_something_else_with("{ args }" )
     end
   end
 end
