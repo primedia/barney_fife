48a698be660a7d131fa146a07deac63341541659
Colin Rymer <colin.rymer@gmail.com>
2014-05-15 10:04:10 -0400

    cleaner validations for repository

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -1,18 +1,9 @@
 class Repository < ActiveRecord::Base
   validates :name, :organization, presence: true
-  validate :repo_full_name_is_unique
+  validates_uniqueness_of :name, scope: :organization
 
   def full_name
     "#{organization}/#{name}"
   end
 
-  def repo_full_name_is_unique
-    unless full_name_unique?
-      errors.add(:name, 'There is already a repository with that name for this organization.')
-    end
-  end
-
-  def full_name_unique?
-    self.class.where(organization: organization, name: name).count == 0
-  end
 end
